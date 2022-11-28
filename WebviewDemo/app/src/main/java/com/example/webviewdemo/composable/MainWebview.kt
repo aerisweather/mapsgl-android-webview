4d65c0d59f5fa518f3b429dc3919fa2f9117583b
CT Yeung <chi.yeung@vaisala.com>
2022-11-28 10:02:50 -0600

    warning dialog
    Add warning dialog for initial condition.

--- WebviewDemo/app/src/main/java/com/example/webviewdemo/composable/MainWebview.kt
+++ WebviewDemo/app/src/main/java/com/example/webviewdemo/composable/MainWebview.kt
@@ -1,9 +1,22 @@
 package com.example.webviewdemo.composable
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.Dialog
 import com.example.mapsglwebviewlib.MapsGLWebview
 import com.example.mapsglwebviewlib.interop.JSBuilder
 import com.example.webviewdemo.*
@@ -32,6 +45,7 @@ fun ComposeMain(viewModel: MainViewModel) {
                 ComposeAnimationControl(viewModel)
             }
             ComposeLegend(viewModel.openLegends)
+            ComposeSecretDialog(viewModel)
         },
     )
 }
@@ -56,4 +70,41 @@ fun ComposeWebView(viewModel: MainViewModel) {
     }, update = {
         it.load()
     })
+}
+
+
+@Composable
+fun ComposeSecretDialog(viewModel: MainViewModel) {
+    /*
+     * Display annoying dialog if aeris-id and aeris-secret not implemented
+     */
+
+    val id = stringResource(id = com.example.mapsglwebviewlib.R.string.aerisapi_client_id)
+    val secret = stringResource(id = com.example.mapsglwebviewlib.R.string.aerisapi_client_secret)
+    if (id.lowercase().contains("your") || secret.lowercase().contains("your")) {
+        Dialog(onDismissRequest = { /*TODO*/ }) {
+            Card() {
+                Column() {
+                    Row(modifier = Modifier.padding(10.dp, 10.dp, 10.dp, 10.dp)) {
+                        Icon(
+                            painterResource(
+                                R.drawable.ic_warning
+                            ),
+                            "animate",
+                            modifier = Modifier.size(35.dp)
+                        )
+
+                        Text(
+                            text = stringResource(id = R.string.permission),
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier.padding(5.dp, 5.dp, 5.dp, 5.dp)
+                        )
+                    }
+                    Row(modifier = Modifier.padding(10.dp, 10.dp, 10.dp, 10.dp)) {
+                        Text(text = stringResource(id = R.string.permission_desc))
+                    }
+                }
+            }
+        }
+    }
 }
